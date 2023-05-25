d1f4505525cd281083d892a6d9ea89b632da71fb
q <soldatovv454@gmail.com>
2023-05-25 15:51:50 +0300

    refactoring

---
+++ kube_engine/core/render_engine/render.cc
@@ -0,0 +1,15 @@
+/* =============================================================== 
+// KubeEngine is an open source game engine.
+// Copyright (C) 2023 The KubeEngine authors
+//
+// Licensed under the Apache License v.2;
+// You may not use this file except in compliance with the License
+// 
+// https://github.com/VaisonE/KubeEngine/blob/main/license
+// 
+// All Rights Reserved
+=============================================================== */
+
+// Download Vulkan SDK
+#include <vulkan/vulkan.h>
+
