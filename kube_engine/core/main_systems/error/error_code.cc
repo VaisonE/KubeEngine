f186e4cbbe920081a360f5a0022512e12b975d37
q <soldatovv454@gmail.com>
2023-05-28 06:37:54 +0300

    Adding a Utilities Directory

---
+++ kube_engine/core/main_systems/error/error_code.cc
@@ -0,0 +1,20 @@
+// ==================================================================
+// KubeEngine is an open source game engine.
+// Copyright (C) 2023 The KubeEngine authors
+//
+// Licensed under the Apache License v.2;
+// You may not use this file except in compliance with the License
+// 
+// https://github.com/VaisonE/KubeEngine/blob/main/license
+// 
+// All Rights Reserved
+// ==================================================================
+// error.cc                                                          
+// ==================================================================
+
+#pragma once
+#include <string>
+
+enum ERROR_CODE {
+    UNKNOWN_ERROR = 0
+};
